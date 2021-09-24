22e949e27a63beea3c5bb4c59bb90999bef4d998
Anarhist71 <48045805+Anarhist71@users.noreply.github.com>
2021-09-24 12:03:09 +0300

    Update First Program.java

--- javaCORE/First Program.java
+++ javaCORE/First Program.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-  publicclassFirstProgram{
+public class FirstProgram{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число: ");
