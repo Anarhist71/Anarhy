74a508bf3daa67f0666b7467fdeb45a4324ecaea
ANARHIST JIMBO <48045805+Anarhist71@users.noreply.github.com>
2021-09-30 21:46:32 +0300

    Update 1.3 Program.java

--- javaCORE/1.3 Program.java
+++ javaCORE/1.3 Program.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Third Program {
+public class ThirdProgram {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число: ");
